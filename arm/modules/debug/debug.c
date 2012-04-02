984be2fc924d147f0fff5bb2162fac337b03058a
Kim Lindberg Schwaner <kim.schwaner@gmail.com>
2012-04-03 01:27:28 +0200

    Accidently ignored debug module

---
+++ arm/modules/debug/debug.c
@@ -0,0 +1,15 @@
+
+
+/**
+ * Error function used by the Stellaris driver library.
+ *
+ * Having it here won't hurt anyone even if the Stellaris lib
+ * is not used.
+ */
+void __error__(char *pcFilename, unsigned long ulLine)
+{
+  while(1)
+  {
+
+  }
+}
