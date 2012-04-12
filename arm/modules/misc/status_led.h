f803e4198ece633cf23a44d010d414e1141c0c89
Kim Lindberg Schwaner <kim.schwaner@gmail.com>
2012-04-12 06:51:06 +0200

    Using portTICK_RATE_MS to enter delay in millisec

--- arm/modules/misc/status_led.h
+++ arm/modules/misc/status_led.h
@@ -2,13 +2,12 @@
 #ifndef STATUS_LED_H
 #define STATUS_LED_H
 
-#define STATUS_LED_DELAY_TICKS  250
+#define STATUS_LED_DELAY_TICKS  (portTICK_RATE_MS * 2.5)
 #define STATUS_LED_STACK_SIZE   configMINIMAL_STACK_SIZE
 #define STATUS_LED_PRIORITY     tskIDLE_PRIORITY
 
 void status_led_task(void *pvParameters);
 BOOLEAN status_led_task_init(void);
 void status_led_init_hw(void);
-
 
 #endif /* STATUS_LED_H */
