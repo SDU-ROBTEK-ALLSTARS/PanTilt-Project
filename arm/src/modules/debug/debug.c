503ded1cb093a311478172e72fc79bc1145631e5
Kim Lindberg Schwaner <kim.schwaner@gmail.com>
2012-05-02 20:09:28 +0200

    Slightly modifies debug module.
    It is working the stellaris lib correctly

--- arm/src/modules/debug/debug.c
+++ arm/src/modules/debug/debug.c
@@ -1,10 +1,13 @@
 
+/* StellarisWare drvs */
+#include "driverlib/debug.h"
+
+/* This modules header */
+#include "debug.h"
 
 /**
  * Error function used by the Stellaris driver library.
  *
- * Having it here won't hurt anyone even if the Stellaris lib
- * is not used.
  */
 void __error__(char *pcFilename, unsigned long ulLine)
 {
