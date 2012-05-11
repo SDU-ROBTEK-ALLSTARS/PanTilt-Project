c737576670b766aaf5facf10dbc8ff8812f16b20
Kim Lindberg Schwaner <kim.schwaner@gmail.com>
2012-05-11 11:44:11 +0200

    Updated SPI test

--- arm/src/test/comm/spi_test.c
+++ arm/src/test/comm/spi_test.c
@@ -30,6 +30,7 @@
  * The SPI test
  */
 
+#define SPI_TEST_NUM_TASKS  8
 
 
 /**
@@ -70,13 +71,10 @@ char* itoa(int value, char* result, int base)
 }
 
 
-#define SPI_TEST_NUM_TASKS 1
-
-
-
 static const char str_loopback_succes[] = " Data loop-back SUCCESS!\n";
 static const char str_loopback_fail[] = " Data loop-back FAIL!\n";
 
+static INT32U spi_test_num_loops;
 
 static struct spi_test_id_t
 {
@@ -87,64 +85,92 @@ static struct spi_test_id_t
 } spi_test_task[SPI_TEST_NUM_TASKS];
 
 
-/* Task */
-#define SPI_TEST_TASK(TASK_NUM)                                                                                  \
-  void spi_test_task_##TASK_NUM(void *params)                                                                    \
-  {                                                                                                              \
-    const char write_buf[] = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz";          \
-    char read_buf[sizeof(write_buf)];                                                                            \
-    int num = 20;                                                                                                \
-                                                                                                                 \
-                                                                                                                 \
-    uart_write((INT8U *) spi_test_task[TASK_NUM].name, strlen(spi_test_task[TASK_NUM].name), portMAX_DELAY);     \
-    uart_write((INT8U *) " started\n", 9, portMAX_DELAY);                                                        \
-                                                                                                                 \
-                                                                                                                 \
-    spi_write_from_task((INT8U *) write_buf, num, portMAX_DELAY);                                                \
-                                                                                                                 \
-                                                                                                                 \
-    spi_read_from_task((INT8U *) read_buf, num, portMAX_DELAY);                                                  \
-                                                                                                                 \
-    if (strncmp(write_buf, read_buf, num) == 0)                                                                  \
-    {                                                                                                            \
-      uart_write((INT8U *) spi_test_task[TASK_NUM].name, strlen(spi_test_task[TASK_NUM].name), portMAX_DELAY);   \
-      uart_write((INT8U *) str_loopback_succes, strlen(str_loopback_succes), portMAX_DELAY);                     \
-    }                                                                                                            \
-    else                                                                                                         \
-    {                                                                                                            \
-      uart_write((INT8U *) spi_test_task[TASK_NUM].name, strlen(spi_test_task[TASK_NUM].name), portMAX_DELAY);   \
-      uart_write((INT8U *) str_loopback_fail, strlen(str_loopback_fail), portMAX_DELAY);                         \
-                                                                                                                 \
-      uart_write((INT8U *) "\n", 1, portMAX_DELAY);                                                              \
-      uart_write((INT8U *) write_buf, num, portMAX_DELAY);                                                       \
-      uart_write((INT8U *) "\n", 1, portMAX_DELAY);                                                              \
-      uart_write((INT8U *) read_buf, num, portMAX_DELAY);                                                        \
-    }                                                                                                            \
-                                                                                                                 \
-    vTaskDelete(NULL);                                                                                           \
+/* Macro to easily expand into more functions/tasks */
+#define SPI_TEST_TASK(TASK_NUM)                                                                \
+  /* */                                                                                        \
+  void spi_test_task_##TASK_NUM(void *params)                                                  \
+  {                                                                                            \
+    INT32U taskNameLen = strlen(spi_test_task[TASK_NUM].name);                                 \
+    INT8U readBuf[2];                                                                          \
+    INT8U writeBuf[2];                                                                         \
+    INT32U result = 0;                                                                         \
+                                                                                               \
+    vTaskDelay((portTickType) (10 * TASK_NUM));                                                \
+                                                                                               \
+    uart_write((INT8U *) spi_test_task[TASK_NUM].name, taskNameLen, portMAX_DELAY);            \
+    uart_write((INT8U *) " started\n", 10, portMAX_DELAY);                                     \
+                                                                                               \
+    for (INT32U i=0; i<spi_test_num_loops; i++)                                                \
+    {                                                                                          \
+      writeBuf[0] = (0x90 + TASK_NUM);                                                         \
+      writeBuf[1] = (i + TASK_NUM);                                                            \
+      spi_write_from_task(writeBuf, 2, portMAX_DELAY);                                         \
+      spi_read_from_task(readBuf, 2, portMAX_DELAY);                                           \
+                                                                                               \
+      writeBuf[0] = (0x10 + TASK_NUM);                                                         \
+      writeBuf[1] = 0x00;                                                                      \
+      spi_write_from_task(writeBuf, 2, portMAX_DELAY);                                         \
+      spi_read_from_task(readBuf, 2, portMAX_DELAY);                                           \
+                                                                                               \
+      if (readBuf[1] == (i + TASK_NUM))                                                        \
+      {                                                                                        \
+        result++;                                                                              \
+      }                                                                                        \
+    }                                                                                          \
+                                                                                               \
+    if (result == spi_test_num_loops)                                                          \
+    {                                                                                          \
+      uart_write((INT8U *) spi_test_task[TASK_NUM].name, taskNameLen, portMAX_DELAY);          \
+      uart_write((INT8U *) str_loopback_succes, strlen(str_loopback_succes), portMAX_DELAY);   \
+    }                                                                                          \
+    else                                                                                       \
+    {                                                                                          \
+      uart_write((INT8U *) spi_test_task[TASK_NUM].name, taskNameLen, portMAX_DELAY);          \
+      uart_write((INT8U *) str_loopback_fail, strlen(str_loopback_fail), portMAX_DELAY);       \
+    }                                                                                          \
+                                                                                               \
+    uart_write((INT8U *) spi_test_task[TASK_NUM].name, taskNameLen, portMAX_DELAY);            \
+    uart_write((INT8U *) " stopped\n", 10, portMAX_DELAY);                                     \
+    while(1)                                                                                   \
+    {                                                                                          \
+    }                                                                                          \
   }
 
-
-/* Register task pointer */
-#define SPI_TEST_REG_PTR(TASK_NUM) \
+/* To register task pointer */
+#define SPI_TEST_REG_PTR(TASK_NUM) /* */ \
   spi_test_task[TASK_NUM].ptr = &spi_test_task_##TASK_NUM;
 
-
 SPI_TEST_TASK(0)
-//SPI_TEST_TASK(1)
-//SPI_TEST_TASK(2)
+SPI_TEST_TASK(1)
+SPI_TEST_TASK(2)
+SPI_TEST_TASK(3)
+SPI_TEST_TASK(4)
+SPI_TEST_TASK(5)
+SPI_TEST_TASK(6)
+SPI_TEST_TASK(7)
 
-void spi_test_start(void)
+void spi_test_start(uart_packet_t *packet)
 {
-  char name_number[4];
+  char number[4] = {'\0'};
 
-  /* Enable SPI loopback mode */
-  SSI0_CR1_R |= SSI_CR1_LBM;
+  /* Set number of loops */
+  spi_test_num_loops = packet->data[0];
+
+  /* Signal start and confirm nr of loops */
+  itoa(spi_test_num_loops, number, 10);
+  uart_write((INT8U *) "Starting SPI test (", 19, portMAX_DELAY);\
+  uart_write((INT8U *) number, sizeof(number), portMAX_DELAY);
+  uart_write((INT8U *) " loops)\n", 8, portMAX_DELAY);\
 
   /* Fill in task addresses */
   SPI_TEST_REG_PTR(0)
-  //SPI_TEST_REG_PTR(1)
-  //SPI_TEST_REG_PTR(2)
+  SPI_TEST_REG_PTR(1)
+  SPI_TEST_REG_PTR(2)
+  SPI_TEST_REG_PTR(3)
+  SPI_TEST_REG_PTR(4)
+  SPI_TEST_REG_PTR(5)
+  SPI_TEST_REG_PTR(6)
+  SPI_TEST_REG_PTR(7)
 
   /* Create tasks */
   for (int i=0; i<SPI_TEST_NUM_TASKS; i++)
@@ -153,9 +179,9 @@ void spi_test_start(void)
     {
       spi_test_task[i].id = i;
 
-      strncpy(spi_test_task[i].name, "SPI_TASK_", configMAX_TASK_NAME_LEN);
-      itoa(i, name_number, 10);
-      strncat(spi_test_task[i].name, name_number, sizeof(name_number));
+      strncpy(spi_test_task[i].name, "SPI_TEST_", configMAX_TASK_NAME_LEN);
+      itoa(i, number, 10);
+      strncat(spi_test_task[i].name, number, strlen(number));
 
       xTaskCreate(spi_test_task[i].ptr, (signed portCHAR *) spi_test_task[i].name, (configMINIMAL_STACK_SIZE + 40), NULL, tskIDLE_PRIORITY, spi_test_task[i].handle);
     }
