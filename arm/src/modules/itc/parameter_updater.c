11673f3c01e663b9d0552095b87c661a9df858e0
Kim Lindberg Schwaner <kim.schwaner@gmail.com>
2012-05-14 20:05:46 +0200

    Fixes readBuf shifted data. Registers SPI user with spi_register_task()

--- arm/src/modules/itc/parameter_updater.c
+++ arm/src/modules/itc/parameter_updater.c
@@ -33,31 +33,51 @@ void param_updater_task(void *params)
                          ADDRESS_POS_A_MSB, ADDRESS_POS_A_LSB,
                          ADDRESS_POS_B_MSB, ADDRESS_POS_B_LSB,
                          ADDRESS_VEL_A_MSB, ADDRESS_VEL_A_LSB,
-                         ADDRESS_VEL_B_MSB, ADDRESS_VEL_B_LSB,
-                         (ADDRESS_AUX_FROM_ARM | 0x80), ADDRESS_AUX_TO_ARM,
-                         0x00};
+                         ADDRESS_VEL_B_MSB, ADDRESS_VEL_B_LSB
+                         /*(ADDRESS_AUX_FROM_ARM | 0x80), ADDRESS_AUX_TO_ARM,
+                         0x00*/};
   INT8U readBuf[sizeof(transmitBuf)];
   INT32S temp;
 
+  /* Registers this task to use the SPI */
+  spi_register_task(NULL);
+
   while(1)
   {
-
+    /* Get new PWM values */
     temp = parameter(POP, PAN_PWM_P);
-    transmitBuf[2] = (INT8U) (temp >> 8);
-    transmitBuf[4] = (INT8U) temp;
+    transmitBuf[1] = (INT8U) (temp >> 8);
+    transmitBuf[3] = (INT8U) temp;
 
     temp = parameter(POP, TILT_PWM_P);
-    transmitBuf[2] = (INT8U) (temp >> 8);
-    transmitBuf[4] = (INT8U) temp;
+    transmitBuf[5] = (INT8U) (temp >> 8);
+    transmitBuf[7] = (INT8U) temp;
 
+    /* Send array of addresses and new PWM vals. Read in the response in
+     * the readBuf buffer. */
     spi_write_from_task(transmitBuf, sizeof(transmitBuf), portMAX_DELAY);
-    spi_read_from_task(readBuf, sizeof(readBuf), portMAX_DELAY);
+    spi_read_from_task(readBuf, sizeof(transmitBuf), portMAX_DELAY);
 
-    parameter(PUSH, PAN_POSITION_P, (INT32S) ((readBuf[8] << 8) + readBuf[9]));
-    parameter(PUSH, TILT_POSITION_P, (INT32S) ((readBuf[10] << 8) + readBuf[11]));
-    parameter(PUSH, PAN_VELOCITY_P, (INT32S) ((readBuf[12] << 8) + readBuf[13]));
-    parameter(PUSH, TILT_VELOCITY_P, (INT32S) ((readBuf[14] << 8) + readBuf[15]));
-    parameter(PUSH, FREE_P, (INT32S) readBuf[18]);
+    /* Reading whole arrays seems to be working as inteded now. In case
+     * that goes wrong, try this: */
+    /*
+    for (INT32U i=0; i<sizeof(transmitBuf); i++)
+    {
+      spi_write_from_task(&transmitBuf[i], 1, portMAX_DELAY);
+      spi_read_from_task(&readBuf[i], 1, portMAX_DELAY);
+    }
+    */
+
+    /* Update "our" values with the updated ones from the FPGA */
+    parameter(PUSH, PAN_POSITION_P, (INT32S) ((readBuf[9] << 8) | readBuf[10]));
+    parameter(PUSH, TILT_POSITION_P, (INT32S) ((readBuf[11] << 8) | readBuf[12]));
+
+
+    /* TODO
+    parameter(PUSH, PAN_VELOCITY_P, (INT32S) ((readBuf[13] << 8) | readBuf[14]));
+    parameter(PUSH, TILT_VELOCITY_P, (INT32S) ((readBuf[15] << 8) | readBuf[16]));
+	  parameter(PUSH, FREE_P, (INT32S) readBuf[]);
+	  */
 
     vTaskDelay(PAR_UPDATER_TASK_FREQUENCY);
   }
@@ -65,7 +85,14 @@ void param_updater_task(void *params)
 
 BOOLEAN par_updater_init(void)
 {
-  return xTaskCreate(param_updater_task, (signed portCHAR *) "PAR_UPDATE", PAR_UPDATER_TASK_STACK_SIZE, NULL, PAR_UPDATER_TASK_PRIORITY, NULL);
+  if (xTaskCreate(param_updater_task, (signed portCHAR *) "PAR_UPDATE", PAR_UPDATER_TASK_STACK_SIZE, NULL, PAR_UPDATER_TASK_PRIORITY, NULL) == pdPASS)
+  {
+    return TRUE;
+  }
+  else
+  {
+    return FALSE;
+  }
 }
 
 
