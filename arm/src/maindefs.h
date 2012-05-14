62b3c16c12f62c76da170fabf26125c326aab150
Kim Lindberg Schwaner <kim.schwaner@gmail.com>
2012-05-14 20:09:00 +0200

    Task prio's relative to FreeRTOS idle task. Sets some block times higher.

--- arm/src/maindefs.h
+++ arm/src/maindefs.h
@@ -6,15 +6,16 @@
 #define 	DEFAULT_STACK  				( ( unsigned short ) 80 )
 #define		LARGE_STACK					( ( unsigned short ) 180 )
 #define 	NAME(x)						( signed portCHAR * )x
-#define 	PRIORITY_HIGH				3
-#define 	PRIORITY_USER				2
-#define		PRIORITY_LOW				1
+#define 	PRIORITY_HIGH				(tskIDLE_PRIORITY + 2)
+#define 	PRIORITY_USER				(tskIDLE_PRIORITY + 1)
+#define		PRIORITY_LOW				(tskIDLE_PRIORITY)
 
-#define 	YIELD_TIME_DREH_T			10
-#define		YIELD_TIME_LCD_T			1
-#define 	YIELD_TIME_MENU_T			5
-#define 	YIELD_TIME_CONTROL_T		100
+#define 	YIELD_TIME_DREH_T			50
+#define		YIELD_TIME_LCD_T			2
+#define 	YIELD_TIME_MENU_T			100
+#define 	YIELD_TIME_CONTROL_T		10
 #define		YIELD_TIME_BLINK_T			350
+#define		YIELD_TIME_NUMPAD_T			40
 
 enum task_handle_names
 {
@@ -24,13 +25,6 @@ enum task_handle_names
 	NUMPAD_T,
 	CONTROL_T,
 	BLINK_T,
-
-	FUELING_T,
-	PWM_T,
-	BUTTONS_T,
-	RX_T,
-	TX_T,
-	HALL_T
 };
 
 #endif /*MAINDEFS_H*/
